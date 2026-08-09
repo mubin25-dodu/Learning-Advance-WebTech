4281f2d682c4cfb69a73c98a994f428ffad5a63f
mubin25-dodu <mubin9516@gmail.com>
2026-08-09 11:20:29 +0600

    Add axios and daisyui dependencies, implement login form with email verification

--- FrontEnd/Practice/signupTest/src/App.tsx
+++ FrontEnd/Practice/signupTest/src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Authpage from './Components/SignUpAndRegistration'
 import './App.css'
-import "tailwindcss";
 
 function App() {
 
   return (
     <>
-      
+      <Authpage/>
     </>
   )
 }
--- FrontEnd/Practice/signupTest/src/Components/SignUpAndRegistration.tsx
+++ FrontEnd/Practice/signupTest/src/Components/SignUpAndRegistration.tsx
@@ -1,7 +1,66 @@
- function Login() {
+import axios from "axios";
+
+ function Authpage() {
     return (
         <>
-        <div></div>
+        {/* main outerlayer */}
+        <div className="bg-white w-[100vw] h-[100vh] flex items-center justify-center">
+            
+            {/* the contaainer */}
+            <div className="bg- w-[50vw] h-[50vh] rounded-2xl flex " style={{boxShadow:'0px 0px 20px 1px'}}> 
+                {/* slider */}
+                <form id="loginform" className="flex items-center justify-center w-[25vw] h-[50vh] text-black ml-auto" action="">
+                    <div className=" flex items-start justify-center flex-col">
+                        <div className="flex items-start flex-col">
+                            <div className="items-start"><label  htmlFor="">Enter Email:</label></div>
+                            <div><input className="border" type="email" name="" id="" /></div>
+                        </div>
+                        <div className="flex items-start flex-col">
+                            <div className="items-start"><label  htmlFor="">Enter Password:</label></div>
+                            <div><input className="border " type="password" name="" id="" /></div>
+                        </div>
+                        <span className="text-red disabled"></span>
+                        <a className="text-red-800 cursor-pointer font-medium ">Forget Password?</a>
+                        <button className="btn btn-neudival mt-2" type="submit">Login</button>
+                    </div>
+                </form>
+                <form id="loginform" className="flex items-center justify-center w-[25vw] h-[50vh] text-black ml-auto" action="">
+                    <div className=" flex items-start justify-center flex-col">
+                        <div className="flex items-start flex-col">
+                            <div className="items-start"><label  htmlFor="">Enter Email:</label></div>
+                            <div><input className="border" type="email" name="" id="VerifyEmail" /></div>
+                        </div>
+                        <span className="text-red" id="emailerrorverify"></span>
+                        <button className="btn btn-neudival mt-2" type="button" onClick={()=>VerifyEmail()}>Send mail</button>
+                    </div>
+                </form>
+
+                {/* <div className="bg-blue-700 w-[25vw] h-[50vh]" style={{ borderRadius: '100px 16px 16px  ' }}></div> */}
+
+            </div>
+        </div>
         </>
     );
  }
+
+ async function VerifyEmail(){
+    const emailInput = document.getElementById('VerifyEmail') as HTMLInputElement | null;
+    const errorspan = document.getElementById('emailerrorverify') as HTMLInputElement | null;
+
+    console.log(import.meta.env.VITE_HELLO);
+    
+    if (emailInput && emailInput.value) {
+        console.log('Email entered:', emailInput.value);
+        const getres = await axios.post(`${import.meta.env.VITE_API_URL}auth/Verifyemail` , emailInput.value);
+
+        console.log(getres);
+        
+    } else {
+        if (errorspan) { 
+            errorspan.value = "";
+            errorspan.textContent = 'Please enter your email';
+        }
+    }
+ }
+
+ export default Authpage
